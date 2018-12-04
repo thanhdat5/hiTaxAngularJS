e84a870c67a474b79a5094013adb3ce3647dd3bd
thanhdat5 <meodc94@gmail.com>
2018-12-05 00:01:06 +0700

    Add project files.

---
+++ hiTaxAngularJS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hiTaxAngularJS.Startup))]
+namespace hiTaxAngularJS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
